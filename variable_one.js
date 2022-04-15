317ce5fd7e660d63b78c10b4d3dcbe95f06fa028
Gino Serge <gino.ravanty@gmail.com>
2022-04-15 16:56:43 +0300

    first commit

---
+++ variable_one.js
@@ -0,0 +1,9 @@
+let mot="swim";
+console.log(mot+" felicia");
+let num=20;
+num=num*2;
+console.log(num);
+let numberOfApple=99;
+let appleLeft=numberOfApple-1;
+console.log(appleLeft);
+console.log(numberOfApple);
